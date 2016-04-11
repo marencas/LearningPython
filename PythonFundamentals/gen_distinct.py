2f55f7627c524a611571bfa854659036f4e42019
Martin Castillo <CastilloM@SMH.CA>
2016-04-11 17:44:09 -0400

    More code added for python fundamentals

---
+++ PythonFundamentals/gen_distinct.py
@@ -0,0 +1,28 @@
+"""Another module for understanding generator execution."""
+
+
+def distinct(iterable):
+    """Return unique items by eliminating duplicates.
+
+    Args:
+        iterable: The source series.
+
+    Returns:
+        Unique elements in order from 'iterable'
+    """
+    seen = set()
+    for item in iterable:
+        if item in seen:
+            continue
+        yield item
+        seen.add(item)
+
+
+def run_distinct():
+    items = [5,7,7,6,5,5,4]
+    for item in distinct(items):
+        print(item)
+
+
+if __name__ == '__main__':
+    run_distinct()
